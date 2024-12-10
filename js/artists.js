063d5c73d0a4056192ca961f043e25fb2b0edfdb
Nguyen Duong Truong Phuc <nguyenduongtruongphuc@gmail.com>
2024-12-10 14:03:20 -0500

    Create artists.js

---
+++ js/artists.js
@@ -0,0 +1,52 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    artistId: "AID-1111",
+    name: "Central Cee",
+    image: "https://i.scdn.co/image/ab6761610000e5eb5fe7cabb8de1c0266cd2179c", 
+    urls: [
+      {url: "https://open.spotify.com/artist/5H4yInM5zmHqpKIoMNAx4r", name: "Spotify", logo: "https://m.media-amazon.com/images/I/51rttY7a+9L.png"},
+      {url: "https://www.youtube.com/@CentralCee", name: "YouTube", logo: "https://static.vecteezy.com/system/resources/thumbnails/023/986/480/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"},
+      {url: "https://www.facebook.com/centralcee", name: "Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"},
+      {url: "https://www.instagram.com/centralcee", name: "Instagram", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png"}
+    ]
+  },
+  {
+    artistId: "AID-2222",
+    name: "Eminem",
+    image: "https://www.zastavki.com/pictures/originals/2017Music_Popular_singer_rapper_Eminem_photo_on_white_background_117582_.jpg",
+    urls: [
+      {url: "https://open.spotify.com/artist/7dGJo4pcD2V6oG8kP0tJRR", name: "Spotify", logo: "https://m.media-amazon.com/images/I/51rttY7a+9L.png"},
+      {url: "https://www.youtube.com/@eminem", name: "YouTube", logo: "https://static.vecteezy.com/system/resources/thumbnails/023/986/480/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"},
+      {url: "https://www.facebook.com/eminem", name: "Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"},
+      {url: "https://www.instagram.com/eminem", name: "Instagram", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png"}
+    ]
+  },
+  {
+    artistId: "AID-3333",
+    name: "Chance The Rapper",
+    image: "https://hips.hearstapps.com/hmg-prod/images/chance_the_rapper_photo_by_matthew_eisman_getty_images_entertainment_getty_170863100.jpg?crop=1xw:1.0xh;center,top&resize=640:*",
+    urls: [
+      {url: "https://open.spotify.com/artist/1anyVhU62p31KFi8MEzkbf", name: "Spotify", logo: "https://m.media-amazon.com/images/I/51rttY7a+9L.png"},
+      {url: "https://www.youtube.com/@ChanceTheRapper", name: "YouTube", logo: "https://static.vecteezy.com/system/resources/thumbnails/023/986/480/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"},
+      {url: "https://www.facebook.com/chancetherapper", name: "Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"},
+      {url: "https://www.instagram.com/chancetherapper", name: "Instagram", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png"}
+    ]
+  },
+  {
+    artistId: "AID-4444",
+    name: "Drake",
+    image: "https://i.scdn.co/image/ab6761610000e5eb4293385d324db8558179afd9",
+    urls: [
+      {url: "https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4", name: "Spotify", logo: "https://m.media-amazon.com/images/I/51rttY7a+9L.png"},
+      {url: "https://www.youtube.com/channel/UCByOQJjav0CUDwxCk-jVNRQ", name: "YouTube", logo: "https://static.vecteezy.com/system/resources/thumbnails/023/986/480/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"},
+      {url: "https://www.facebook.com/Drake", name: "Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"},
+      {url: "https://www.instagram.com/champagnepapi/", name: "Instagram", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png"}
+    ]
+  }
+];
